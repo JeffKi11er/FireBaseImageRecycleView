65e36959cda71a2e3bdd60942dc7e9dfa935961d
JeffKi11er <fillaytion23@gmail.com>
2020-04-12 20:44:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebaseimagerecycleview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebaseimagerecycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
