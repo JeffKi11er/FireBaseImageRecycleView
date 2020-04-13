f0d771b1497fa41655899d93b6544ec35fc088d6
JeffKi11er <fillaytion23@gmail.com>
2020-04-13 17:23:02 +0700

    the first structure of Upload class

--- app/src/main/java/com/example/firebaseimagerecycleview/organization/Upload.java
+++ app/src/main/java/com/example/firebaseimagerecycleview/organization/Upload.java
@@ -1,4 +1,34 @@
 package com.example.firebaseimagerecycleview.organization;
 
 public class Upload {
+    private String mName ;
+    private String ImageUrl;
+
+    public Upload() {
+
+    }
+
+    public Upload(String mName, String imageUrl) {
+        if (mName.trim().equals("")){
+            mName = "No name";
+        }
+        this.mName = mName;
+        ImageUrl = imageUrl;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getImageUrl() {
+        return ImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        ImageUrl = imageUrl;
+    }
 }
