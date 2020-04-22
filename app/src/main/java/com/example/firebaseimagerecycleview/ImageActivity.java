3cceded29db964dd8406398ce848c4a75184b359
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2020-04-22 08:15:30 +0700

    Update Image Activity

--- app/src/main/java/com/example/firebaseimagerecycleview/ImageActivity.java
+++ app/src/main/java/com/example/firebaseimagerecycleview/ImageActivity.java
@@ -1,4 +1,14 @@
 package com.example.firebaseimagerecycleview;
 
-public class ImageActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
 }
