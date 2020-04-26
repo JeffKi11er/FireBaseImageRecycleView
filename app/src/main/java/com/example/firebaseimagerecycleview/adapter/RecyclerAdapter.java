348c845f0cb41cb4e0311eeec28a019ff0533c1e
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2020-04-26 20:14:50 +0700

    Upload Adapter Image

---
+++ app/src/main/java/com/example/firebaseimagerecycleview/adapter/RecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.firebaseimagerecycleview.adapter;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebaseimagerecycleview.R;
+import com.example.firebaseimagerecycleview.organization.Upload;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHolder> {
+    private Context context;
+    private List<Upload>pictures;
+
+    public RecyclerAdapter(Context context, List<Upload> pictures) {
+        this.context = context;
+        this.pictures = pictures;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.image_item,parent,false);
+        return new MyHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        Upload upload = pictures.get(position);
+        holder.tvName.setText(upload.getmName());
+        Picasso.get().load(upload.getImageUrl()).fit().into(holder.imgRecycler);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public static class MyHolder extends RecyclerView.ViewHolder {
+        private TextView tvName;
+        private ImageView imgRecycler;
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = (TextView)itemView.findViewById(R.id.tv_name);
+            imgRecycler = (ImageView)itemView.findViewById(R.id.img_view_upload);
+        }
+    }
+}
